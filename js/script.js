fe47016cc5e83544664ec6ccf21663481efc9066
Clupai8o0 <clupaio4@gmail.com>
2022-08-07 12:32:42 +0300

    updating to data on click

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-let monthIndex = 50;
+let monthIndex = 0;
 
 const colors = [
 	"var(--chrome)",
 	"var(--ie)",
 	"var(--firefox)",
+	"var(--safari)",
 	"var(--opera)",
 	"var(--android)",
 ];
@@ -14,18 +15,27 @@ svg.attr("width", 640).attr("height", 640);
 const pieGroup = svg.append("g").attr("transform", "translate(320, 320)");
 
 function updateGraph() {
-	const pieGenerator = d3.pie();
+	const pieGenerator = d3.pie().sort(null);
 
 	const arcData = pieGenerator(data[monthIndex]);
 	const arcGenerator = d3.arc().innerRadius(200).outerRadius(300);
 
 	const paths = pieGroup.selectAll("path").data(arcData);
 
+	// for new paths
 	paths
 		.enter()
 		.append("path")
 		.attr("d", arcGenerator)
 		.style("fill", (_, i) => colors[i]);
+
+	// for existing paths
+	paths.attr("d", arcGenerator);
 }
 
 updateGraph();
+
+document.querySelector("a.restart").addEventListener("click", () => {
+	monthIndex += 1;
+	updateGraph();
+});
