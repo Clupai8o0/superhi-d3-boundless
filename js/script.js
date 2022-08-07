b84bb37d283c639d063f7c3a60a89182caa845a9
Clupai8o0 <clupaio4@gmail.com>
2022-08-07 12:19:03 +0300

    script js setup

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let monthIndex = 0;
+
+const svg = d3.select("svg");
+svg.attr("width", 640).attr("height", 640);
+
+const pieGroup = svg.append("g");
+
+function updateGraph() {
+  console.log("update graph")
+}
+
+updateGraph();
