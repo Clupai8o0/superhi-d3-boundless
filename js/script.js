d6d584790ed60ca8f2cf490bdc8bddcce48fe340
Clupai8o0 <clupaio4@gmail.com>
2022-08-07 12:22:09 +0300

    basically showing

--- js/script.js
+++ js/script.js
@@ -6,7 +6,16 @@ svg.attr("width", 640).attr("height", 640);
 const pieGroup = svg.append("g");
 
 function updateGraph() {
-  console.log("update graph")
+	pieGroup
+		.selectAll("rect")
+		.data(data[monthIndex])
+		.enter()
+		.append("rect")
+		.attr("x", (_, i) => i * 100)
+		.attr("y", 0)
+		.attr("width", 90)
+		.attr("height", (d) => d)
+		.style("fill", "white");
 }
 
 updateGraph();
